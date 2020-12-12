c070b1b1a51facef28c04983572434f0cdb64a97
alanaalfeche <alanaalfeche@gmail.com>
2020-12-12 13:11:38 -0600

    Add Day 6 Solution

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -1,18 +1,31 @@
-import itertools
+from itertools import groupby
 
 from data import load_day
 
 
-def group_answers(answers):
-    return [list(''.join(g)) for k, g in itertools.groupby(answers, key=lambda x: x == '')]
+def group_answers(custom_answers):
+    return [list(g) for k, g in groupby(custom_answers, key=lambda x: x != '') if k]
 
-def count_yes(group_answers):
-    custom_yes_count = 0
-    for answer in group_answers:
-        if answer: custom_yes_count += len(set(answer))
-    return custom_yes_count
+def count_any_yes(group_answers):
+    any_yes = 0
+    for group in group_answers:
+        any_yes += len(set(''.join(group)))
+    return any_yes
+
+def count_all_yes(group_answers):
+    all_yes = 0
+    for group in group_answers:
+        if len(group) == 1:
+            all_yes += len(group[0])
+        else:
+            for answer in group[0]:
+                if all(answer in person for person in group[1:]):
+                    all_yes += 1
+    return all_yes
 
 custom_answers = load_day(6)
-group_answers = group_answers(custom_answers)
+parsed_data = group_answers(custom_answers)
 # part a
-print(count_yes(group_answers))
+print(count_any_yes(parsed_data))
+# part b
+print(count_all_yes(parsed_data))
